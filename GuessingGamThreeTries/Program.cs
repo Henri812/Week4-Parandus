b57b4ccb60ee7480efd83e042c2db73967e6205a
Henri Heinmäe <henriheinmae@gmail.com>
2022-05-19 23:08:34 +0300

    add new projects

---
+++ GuessingGamThreeTries/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GuessingGamThreeTries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm genereerib juhuslikku numbrit 1 kuni 10;
+            //kasutaja peab seda numbrit ära arvama;
+            //kui kasutaja suutis seda numbrit ära arvata; siis ta on mängu võitnud;
+            //kasutajal on kolm katset, kui kasutaja ei ole suutnud kolme katsega numbrit ära arvata
+            //siis mängu võidab arvuti.
+            //program genereerib juhuslikku numbrit ühe korra.
+
+            Random rnd = new Random();
+            int cpuNumber = rnd.Next(1, 11);
+
+            while ()
+            {
+
+            }
+        }
+    }
+}
---
+++ GuessingGameInfinite/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GuessingGameInfinite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //programm genereerib juhuslikku numbrit 1 kuni 10;
+            //kasutaja peab seda numbrit ära arvama;
+            //kui kasutaja suutis seda numbrit ära arvata; siis ta on mängu võitnud;
+            //katsete arv on piiramatu.
+            //program genereerib juhuslikku numbrit ühe korra.
+
+            Random rnd = new Random();
+            int cpuNumber = rnd.Next(1, 11);
+
+
+
+            Console.WreiteLine("Kena Päeva");
+        }
+    }
+}
